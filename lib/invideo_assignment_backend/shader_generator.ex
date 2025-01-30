8c88bd8645b37e5be2465cef2350745f0b376df0
ayamdobhal <me@iamdobhal.dev>
2025-01-30 06:07:11 +0530

    feat(shader_gen): switch to gemini-1.5-flash

--- lib/invideo_assignment_backend/shader_generator.ex
+++ lib/invideo_assignment_backend/shader_generator.ex
@@ -1,30 +1,45 @@
 defmodule InvideoAssignmentBackend.ShaderGenerator do
-  @openai_api_key System.get_env("OPENAI_API_KEY") || ""
+  @gemini_api_key System.get_env("GEMINI_API_KEY") || ""
 
   @spec generate_shader(String.t()) :: String.t()
   def generate_shader(description) do
     body = %{
-      model: "gpt-4",
-      messages: [%{role: "user", content: "Generate a GLSL shader for: " <> description}]
+      "contents" => [
+        %{
+          "parts" => [
+            %{"text" => "Generate a GLSL shader for: " <> description <> "Return only the code"}
+          ]
+        }
+      ]
     }
 
     headers = [
-      {"Authorization", "Bearer " <> @openai_api_key},
       {"Content-Type", "application/json"}
     ]
 
-    case HTTPoison.post(
-           "https://api.openai.com/v1/chat/completions",
-           Jason.encode!(body),
-           headers
-         ) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: response_body}} ->
-        response_body
-        |> Jason.decode!()
-        |> get_in(["choices", Access.at(0), "message", "content"])
+    url =
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=#{@gemini_api_key}"
 
-      {:error, _} ->
-        "Failed to generate shader"
+    options = [
+      recv_timeout: 300_000
+    ]
+
+    case HTTPoison.post(url, Jason.encode!(body), headers, options) do
+      {:ok,
+       %HTTPoison.Response{
+         status_code: 200,
+         body: response_body
+       }} ->
+        shader_text =
+          response_body
+          |> Jason.decode!()
+          |> get_in(["candidates", Access.at(0), "content", "parts", Access.at(0), "text"])
+
+        shader_text || "// Failed to generate shader"
+
+      {:error, err} ->
+        IO.puts("Request failed due to:" <> err.reason)
+        "// Failed to generate shader"
     end
   end
 end
