181732d327c720bfadfd5614bb0ce25269ede26f
ayamdobhal <me@iamdobhal.dev>
2025-01-30 20:34:17 +0530

    feat: add tests

--- test/invideo_assignment_backend_test.exs
+++ test/invideo_assignment_backend_test.exs
@@ -1,8 +1,29 @@
 defmodule InvideoAssignmentBackendTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Plug.Test
   doctest InvideoAssignmentBackend
 
   test "greets the world" do
     assert InvideoAssignmentBackend.hello() == :world
+  end
+
+  @opts InvideoAssignmentBackend.Router.init([])
+  test "generates a shader" do
+    conn =
+      conn(
+        :post,
+        "/generate_shader",
+        Jason.encode!(%{"description" => "Create a simple gradient from red to blue"})
+      )
+      |> put_req_header("content-type", "application/json")
+
+    conn =
+      InvideoAssignmentBackend.Router.call(
+        conn,
+        @opts
+      )
+
+    assert conn.state == :sent
+    assert conn.status == 200
   end
 end
